87525de3361e88b92251d2bcdd7098298c4f5b95
Adit P Patel <app2116@gmail.com>
2020-01-02 00:46:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+return my_hash=Hash.new()
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  return {dog: "beagle"}
 end
 
 def pioneer
